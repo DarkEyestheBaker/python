a9ffac28730db9b93a11b14d13e3af26a6e352b0
DarkEyesTheBaker <julesdietsch@fioptics.com>
2021-04-07 14:35:55 -0400

    Guessing Game - If/Elif/Else: 2 Tries (recoded again)

--- ProgramFlow/guessinggame.py
+++ ProgramFlow/guessinggame.py
@@ -3,7 +3,9 @@ answer = 5
 print("Please guess a number between 1 and 10: ")
 guess = int(input())
 
-if guess != answer:
+if guess == answer:
+    print("You got it on the first try!")
+else: 
     if guess < answer:
         print("Please guess higher.")
     else:   # guess must be greater than answer
@@ -13,8 +15,7 @@ if guess != answer:
         print("Well, done!  You guessed it!")
     else:
         print("Sorry, you have not guessed correctly.")
-else:
-    print("You got it on the first try!")
+
 
 # if guess < answer:
 #     print("Please guess higher.")
